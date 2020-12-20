d8d34a84517291ade2080b5c2b4bf9c04932d2a6
Karrakis <belot.nicolas@gmail.com>
2020-12-20 22:53:00 +0100

    Ajout fonction de configuration

--- PoshCCat.psm1
+++ PoshCCat.psm1
@@ -13,37 +13,37 @@
 # TODO: Add Guess File content, file Type function                   # 
 ######################################################################
 
-$CSVColors = @("Blue","Green","Red","Yellow","Orange")
+$CSVColors = @("Blue", "Green", "Red", "Yellow", "Orange")
 $CSVDelimColor = "Purple"
 $LogColors = @{
     DateTimeFrontColor = "Blue"
-    ErrorFrontColor = "Red"
-    WarningFrontColor = "Yellow"
-    HexWordFrontColor = "Green"
-    SIDFrontColor = "LightGreen"
-    GuIDFrontColor = "DarkGreen"
-    IPv4FrontColor = "Orange"
+    ErrorFrontColor    = "Red"
+    WarningFrontColor  = "Yellow"
+    HexWordFrontColor  = "Green"
+    SIDFrontColor      = "LightGreen"
+    GuIDFrontColor     = "DarkGreen"
+    IPv4FrontColor     = "Orange"
 }
 
 function Get-ColorizedContent {
     param(
         [Parameter(Mandatory = $true)]
-        [ValidateScript({
-            if( (Test-Path $_ |? { $_ -ne $true} -gt 0)){
-                Throw [System.IO.FileNotFoundException]
-            }else{
-                return $true
-            }
-        })]
+        [ValidateScript( {
+                if ( (Test-Path $_ | ? { $_ -ne $true } -GT 0)) {
+                    Throw [System.IO.FileNotFoundException]
+                } else {
+                    return $true
+                }
+            })]
         [string[]]$Path
     )
     [System.IO.FileInfo]$File = (Resolve-Path  $Path).Path
-    if($File.Extension -eq '.csv'){
+    if ($File.Extension -eq '.csv') {
         return CsvColor -FilePath $File
-    } elseif( $File.Extension -eq '.log') {
+    } elseif ( $File.Extension -eq '.log') {
         return LogColor -FilePath $File
-    }else {
-        return Get-Content -path $File
+    } else {
+        return Get-Content -Path $File
     }
 }
 
@@ -58,14 +58,14 @@ function CsvColor {
         [string]$FilePath
     )
     $MaxColor = $CSVColors.Count
-    $Delimiter =  ((Get-Content -Path $FilePath)[0].ToCharArray() | group |? { ":",";",",","`t" -contains $_.Name } | sort Count | select -first 1 ).Name
-    $csv  = import-csv -delim $Delimiter -Path $FilePath 
+    $Delimiter = ((Get-Content -Path $FilePath)[0].ToCharArray() | group | ? { ":", ";", ",", "`t" -contains $_.Name } | sort Count | select -First 1 ).Name
+    $csv = Import-Csv -delim $Delimiter -Path $FilePath 
     #header
-    $i=0
-    @($csv[0].psobject.Properties.Name |% { $i++; New-Text $_ -ForegroundColor $CSVColors[$i%$MaxColor] -LeaveColor} ) -join (new-text $Delimiter  -ForegroundColor $CSVDelimColori -LeaveColor)
-    $csv |% {
-        $i=0
-        @($_.psobject.Properties.Value |% { $i++; New-Text $_ -ForegroundColor $CSVColors[$i%$MaxColor] -LeaveColor } ) -join (new-text $Delimiter -ForegroundColor $CSVDelimColor -LeaveColor)
+    $i = 0
+    @($csv[0].psobject.Properties.Name | % { $i++; New-Text $_ -ForegroundColor $CSVColors[$i % $MaxColor] -LeaveColor } ) -join (New-Text $Delimiter  -ForegroundColor $CSVDelimColori -LeaveColor)
+    $csv | % {
+        $i = 0
+        @($_.psobject.Properties.Value | % { $i++; New-Text $_ -ForegroundColor $CSVColors[$i % $MaxColor] -LeaveColor } ) -join (New-Text $Delimiter -ForegroundColor $CSVDelimColor -LeaveColor)
     }
 }
 
@@ -86,25 +86,25 @@ function LogColor {
     $SIDRegex = "(?<SID>S-\d-(?:\d+-){1,14}\d+)"
     $GuIDRegex = "(?<GUID>[A-Z0-9]{8}-([A-Z0-9]{4}-){3}[A-Z0-9]{12})"
     $IPv4Regex = "(?<IPV4>([^1-9.][1-9]|[1-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5])(\.([0-9]|[1-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5])){3} )"
-    Get-Content $FilePath |% {
+    Get-Content $FilePath | % {
         $line = $_
-        if( $_ -match $DateRegexp){ 
-            $matches.Values | group |% {
+        if ( $_ -match $DateRegexp) { 
+            $matches.Values | group | % {
                 $line = $line.replace($_.Name, (New-Text -ForegroundColor $LogColors.DateTimeFrontColor -Object $_.Name ).toString() )
             }
         }
-        if( $_ -match $TimeRegexp){ 
-            $matches.Values | group |% {
+        if ( $_ -match $TimeRegexp) { 
+            $matches.Values | group | % {
                 $line = $line.replace($_.Name, (New-Text -ForegroundColor $LogColors.DateTimeFrontColor -Object $_.Name ).toString() )
             }
         }
-        $line = $line -replace $HexWordRegexp,(new-text '${HexWord}' -ForegroundColor $LogColors.HexWordFrontColor ).toString()
-        $line = $line -replace $PathRegexp,('${Before}' + (New-UnderlineText '${Path}' ))
-        $line = $line -replace $SIDRegex, (new-text '${SID}' -ForegroundColor $logColors.SIDFrontColor )
-        $line = $line -replace $GuIDRegex, (new-text '${GUID}' -ForegroundColor $logColors.GuIDFrontColor )
-        $line = $line -replace $IPv4Regex, (new-text '${IPV4}' -ForegroundColor $logColors.IPv4FrontColor )
-        $line = $line -replace "(?<Match>Error)",(new-text '${Match}' -ForegroundColor $LogColors.ErrorFrontColor ).toString()
-        $line = $line -replace "(?<Match>Warning)",(new-text '${Match}' -ForegroundColor $Logcolors.WarningFrontColor ).toString()
+        $line = $line -replace $HexWordRegexp, (New-Text '${HexWord}' -ForegroundColor $LogColors.HexWordFrontColor ).toString()
+        $line = $line -replace $PathRegexp, ('${Before}' + (New-UnderlineText '${Path}' ))
+        $line = $line -replace $SIDRegex, (New-Text '${SID}' -ForegroundColor $logColors.SIDFrontColor )
+        $line = $line -replace $GuIDRegex, (New-Text '${GUID}' -ForegroundColor $logColors.GuIDFrontColor )
+        $line = $line -replace $IPv4Regex, (New-Text '${IPV4}' -ForegroundColor $logColors.IPv4FrontColor )
+        $line = $line -replace "(?<Match>Error)", (New-Text '${Match}' -ForegroundColor $LogColors.ErrorFrontColor ).toString()
+        $line = $line -replace "(?<Match>Warning)", (New-Text '${Match}' -ForegroundColor $Logcolors.WarningFrontColor ).toString()
         $line
     }
 }
@@ -116,4 +116,19 @@ function New-UnderlineText {
     return "$([char]27)[4m$text$([char]27)[24m"
 }
 
+function Set-CCarColor {
+    param(
+        [string[]]$CSVColors
+    )
+}
+
 Export-ModuleMember -Function "Get-ColorizedContent"
+
+######################################################################
+# Analyze                                                            #
+######################################################################
+# PSAvoidUsingCmdletAliases occured 13                               #
+# PSReviewUnusedParameter occured 1                                  #
+# PSUseShouldProcessForStateChangingFunctions occured 2              #
+######################################################################
+
