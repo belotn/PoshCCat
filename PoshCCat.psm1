893a90ae4c033f084a9cfe0a44e34f6971494f66
Karrakis <belot.nicolas@gmail.com>
2020-12-23 16:06:48 +0100

    PRF file is equivalent to ini file

--- PoshCCat.psm1
+++ PoshCCat.psm1
@@ -69,7 +69,7 @@ function Get-ColorizedContent {
         return CsvColor -FilePath $File
     } elseif ( $File.Extension -eq '.log') {
         return LogColor -FilePath $File
-    } elseif ( $File.Extension -eq '.ini' -or $File.Extension -eq '.inf' -or $File.Extension -eq '.ica' -or $File.Extension -eq '.prf' ) {
+    } elseif ( $File.Extension -eq '.ini' -or $File.Extension -eq '.inf' -or $File.Extension -eq '.ica' -or $File.Extension -eq '.prf' -or $File.Extension -eq '.cmw') {
         return IniColor -FilePath $File
     } elseif ( $File.Extension -eq '.conf' -or $File.Extension -eq '.cfg') {
         return ConfigFileColor -FilePath $File
